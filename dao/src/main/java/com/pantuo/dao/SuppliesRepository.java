cee411f2216b3f476457cd8f9c276a95e94b496f
pxh <pxh@4270119f-925b-4bd5-8177-7128d692826f>
2015-05-11 15:21:44 +0000

    素材表增加默认id 1

--- dao/src/main/java/com/pantuo/dao/SuppliesRepository.java
+++ dao/src/main/java/com/pantuo/dao/SuppliesRepository.java
@@ -1,13 +1,29 @@
 package com.pantuo.dao;
 
-import com.pantuo.dao.pojo.JpaContract;
-import com.pantuo.dao.pojo.JpaSupplies;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pantuo.dao.pojo.JpaSupplies;
 
 /**
- * @author tliu
+ * 
+ * 
+ * <b><code>SuppliesRepository</code></b>
+ * <p>
+ * 默认不选择时绑定的特定素材
+ * </p>
+ * <b>Creation Time:</b> 2015年5月11日 下午11:17:21
+ * @author impanxh@gmail.com
+ * @since pantuotech 1.0-SNAPSHOT
  */
 
 public interface SuppliesRepository extends JpaRepository<JpaSupplies, Integer>, QueryDslPredicateExecutor<JpaSupplies> {
+
+	@Modifying
+	@Transactional
+	@Query(nativeQuery = true, value = "insert into supplies (id,created,updated,city,supplies_type) values(1,now(),now(),0,0)")
+	public int insertDefaultSupplies();
 }
