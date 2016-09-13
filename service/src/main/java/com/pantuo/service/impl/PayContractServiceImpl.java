d0e84e6f15f578b34969774027174870cbc31b49
impanxh@gmail.com <impanxh@gmail.com>
2016-09-13 14:32:46 +0800

    coment 合同接口

---
+++ service/src/main/java/com/pantuo/service/impl/PayContractServiceImpl.java
@@ -0,0 +1,47 @@
+package com.pantuo.service.impl;
+
+import java.security.Principal;
+import java.util.List;
+
+import com.pantuo.dao.pojo.JpaOrders;
+import com.pantuo.service.PayContractService;
+
+public class PayContractServiceImpl  implements PayContractService{
+	/**
+	 * 
+	 * 查我的未支付订单
+	 *
+	 * @param principal
+	 * @return
+	 * @since pantuo 1.0-SNAPSHOT
+	 */
+	public List<String> queryMyUnPayOrders(Principal principal){
+		
+	}
+	/**
+	 * 
+	 * 根据订单号查
+	 *
+	 * @param orderId
+	 * @return
+	 * @since pantuo 1.0-SNAPSHOT
+	 */
+	public List<JpaOrders> queryOrders(List<String> orderId){
+		
+	}
+	
+	
+	
+	/**
+	 * 
+	 * 查是不是都属于同一个
+	 *
+	 * @param orderId
+	 * @return
+	 * @since pantuo 1.0-SNAPSHOT
+	 */
+	public boolean checkSameCustom(List<String> orderId){
+		
+	}
+
+}
