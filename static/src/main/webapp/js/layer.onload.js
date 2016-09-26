57f5f721245f7a4aca19d356a899249feabb545e
zrdzt321 <zrdzt321@126.com>
2016-09-26 17:05:19 +0800

    zr

--- static/src/main/webapp/js/layer.onload.js
+++ static/src/main/webapp/js/layer.onload.js
@@ -49,9 +49,9 @@ function bindLayerMouseWithOpen(time){
 	
 }
 function bindLayerMouseOver(){
-	bindLayerMouseOverWithTime(120000);
+	bindLayerMouseOverWithTime(120000); 
 }
 
 $(document).ready(function(){
 	bindLayerMouseOver();
-})
+}) 
